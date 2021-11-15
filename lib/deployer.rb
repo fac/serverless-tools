af2b0f5daf8b344473babcde40a667ce57b1db63
Paul Barber <p.barber1990@gmail.com>
2021-11-15 13:13:26 +0000

    Reduce cognitive complexity of deployer.rb

--- lib/deployer.rb
+++ lib/deployer.rb
@@ -1,32 +1,32 @@
-require 'yaml'
+require "yaml"
 
-require_relative './deployer/aws_lambda_config'
-require_relative './deployer/deployer'
+require_relative "./deployer/aws_lambda_config"
+require_relative "./deployer/deployer"
 
 module Deployer
-  CONFIG_FILE = 'functions.yml'
+  CONFIG_FILE = "functions.yml"
 
   def self.deploy(action:, function: nil)
+    raise "Expected to receive action but action was empty" if action.nil? || action.empty?
+
     lambdas_to_deploy = if function
                           [function]
                         else
                           YAML.load_file(CONFIG_FILE).keys
-                        end
+    end
 
     configs = lambdas_to_deploy.map { |l| AwsLambdaConfig.new(filename: CONFIG_FILE, function_name: l) }
     deployers = configs.map { |c| Deployer.new(c) }
 
-    case action
-    when 'build'
-      deployers.each { |d| d.build }
-    when 'push'
-      deployers.each { |d| d.push }
-    when 'update'
-      deployers.each { |d| d.update }
-    when 'deploy'
-      deployers.each { |d| d.deploy }
-    else
+    run_action(action: action, deployers: deployers)
+  end
+
+  def self.run_action(action:, deployers:)
+    deployers.each do deployer
+                      deployer.send(action)
+    rescue NoMethodError, ArgumentError => e
       puts "Given action not known! Got #{action}, expected one of [build, push, update, deploy]"
+      puts e.message
     end
   end
 end
