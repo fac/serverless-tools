3f9a6378da739785378e3beff42afb93d44e07dc
Ewa Lipinska <ewa.lipinska@freeagent.com>
2022-11-22 16:48:13 +0000

    Send deployment notifications to Slack
    Adds a new tool for sending deployment notifications to Slack.
    Supports messages for start, success, and failure of a deployment.

---
+++ lib/serverless-tools/notifier.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+require "slack-ruby-client"
+require_relative "./notifier/message"
+require_relative "./notifier/slack_notifier"
+
+module ServerlessTools
+  module Notifier
+    def self.notify(status:, repo_name:, channel:, run_id:)
+      text = Message.new(repo_name: repo_name, run_id: run_id).text_for_status(status)
+
+      SlackNotifier.new.notify(channel: channel, username: repo_name, text: text)
+    end
+  end
+end
---
+++ test/notifier/slack_notifier_test.rb
@@ -0,0 +1,37 @@
+require "minitest/autorun"
+require "mocha/minitest"
+
+require "serverless-tools/notifier/slack_notifier"
+
+module ServerlessTools::Notifier
+  describe SlackNotifier do
+    let(:mock_slack_client) { mock("slack_client") }
+    let(:channel) { "test channel" }
+    let(:username) { "test username" }
+    let(:text) { "test message" }
+
+    subject { SlackNotifier.new(slack_client: mock_slack_client) }
+
+    describe "#notify" do
+      before do
+        mock_slack_client
+        .expects(:chat_postMessage)
+        .with(
+          channel: channel,
+          username: username,
+          text: text,
+          link_names: true,
+          icon_emoji: ":github:"
+        )
+      end
+
+      it "correctly calls the client" do
+        subject.notify(
+          channel: channel,
+          username: username,
+          text: text
+        )
+      end
+    end
+  end
+end
