30e2641c8cd9edf569b0f1550c5fca5f1cb25625
Paul Barber <p.barber1990@gmail.com>
2023-04-12 11:00:53 +0100

    Add git config to resolve #120

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,10 @@
 # `$*` expands the `args` supplied in an `array` individually
 # or splits `args` in a string separated by whitespace.
 echo ">>> Running serverless-tools"
+
+# The following config is required to use the git
+# client within Github Actions. For more information,
+# see here: https://github.com/fac/serverless-tools/issues/120
+git config --global --add safe.directory "$GITHUB_WORKSPACE"
+
 sh -c "serverless-tools $*"
