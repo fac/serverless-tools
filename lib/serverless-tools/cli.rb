9097672b633b958124ac0e92126a6556124899a7
agnesg <agnes.gajda@freeagent.com>
2025-04-07 16:45:51 +0100

    WIP: standalone deployment ops

--- lib/serverless-tools/cli.rb
+++ lib/serverless-tools/cli.rb
@@ -9,6 +9,7 @@ module ServerlessTools
   class CLI < Thor
     desc "comment", "create Github Issue comment body"
     method_option :functions, :type => :string, :aliases => "-f", :default => "{}"
+
     def comment
       system("echo \"comment<<EOF\" >> $GITHUB_OUTPUT")
       Comment.new.build(options[:functions]) do |line|
@@ -20,7 +21,8 @@ module ServerlessTools
     desc "deploy", "publishes and deploys the specified lambda functions"
     method_option :filename, :type => :string, :aliases => "-f", :default => "functions.yml"
     method_option :force, :type => :boolean, :aliases => "-x", :default => false
-    def deploy(action, function=nil)
+
+    def deploy(action, function = nil)
       Deployer.deploy(
         action: action,
         function: function,
@@ -31,6 +33,7 @@ module ServerlessTools
     desc "notify STATUS RUN_ID", "reports deployment status to Slack"
     method_option :repo, :type => :string, :aliases => "-r", :required => true
     method_option :channel, :type => :string, :aliases => "-c", :required => true
+
     def notify(status, run_id)
       Notifier.notify(
         status: status,
@@ -40,7 +43,22 @@ module ServerlessTools
       )
     end
 
+    desc "update", "updates the lambda function image uri"
+    method_option :name, :type => :string, :aliases => "-n", :required => true
+    method_option :image_uri, :type => :string, :aliases => "-i", :required => true
+
+    def update
+      LambdaUpdater.new(
+        client: ::AWS::Lambda::Client.new,
+        config: {
+          name: options[:name],
+          image_uri: options[:image_uri]
+        }
+      )
+    end
+
     desc "version", "prints the version of the library"
+
     def version
       puts VERSION
     end
